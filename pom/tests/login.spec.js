095fa6667846d4958bc98a37f4da72e743e06d37
Guilllermo Oscar <85650228+GuillermoOsc@users.noreply.github.com>
2022-10-26 19:29:31 -0300

    Se implementa patron de diseño pom

--- pom/tests/login.spec.js
+++ pom/tests/login.spec.js
@@ -1,15 +1,11 @@
 import { test, expect } from '@playwright/test';
+const { LoginPage } = require('../pages/login-page');
 
 test('El usuario debe iniciar sesión correctamente', async ({ page }) => {
 
   await page.goto('https://www.saucedemo.com/');
-
-  await page.locator('[data-test="username"]').fill('standard_user');
-
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-
-  await page.locator('[data-test="login-button"]').click();
-
+  const loginPage = new LoginPage(page);
+  await loginPage.enviarFormSesion('standard_user', 'secret_sauce')
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
 });
