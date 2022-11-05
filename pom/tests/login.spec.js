f2567bd341b2dc141dc49bdeaec007f6dddf3296
Guilllermo Oscar <85650228+GuillermoOsc@users.noreply.github.com>
2022-11-05 20:46:15 -0300

    se implementan variables de ambiente

--- pom/tests/login.spec.js
+++ pom/tests/login.spec.js
@@ -1,11 +1,14 @@
 import { test, expect } from '@playwright/test';
 const { LoginPage } = require('../pages/login-page');
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 test('El usuario debe iniciar sesión correctamente', async ({ page }) => {
 
-  await page.goto('https://www.saucedemo.com/');
+  await page.goto(process.env.SAUCEDEMOURL);
   const loginPage = new LoginPage(page);
-  await loginPage.enviarFormSesion('standard_user', 'secret_sauce')
+  await loginPage.enviarFormSesion(process.env.SAUCEDEMOUSER, process.env.SAUCEDEMOPASS);
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
 });
