decaed19cf1a095343b8721ab7c351382808abac
Guillermo Oscar <guillermobianco9090@gmail.com>
2022-11-21 15:35:16 -0300

    Se implementa automatizaciones en login sitio secundario.

--- pom/tests/login.spec.js
+++ pom/tests/login.spec.js
@@ -1,14 +1,35 @@
+// import { test, expect } from '@playwright/test';
+// const { LoginPage } = require('../pages/login-page');
+// import dotenv from 'dotenv'
+
+// dotenv.config()
+
+// test('El usuario debe iniciar sesión correctamente', async ({ page }) => {
+
+//   await page.goto(process.env.SAUCEDEMOURL);
+//   const loginPage = new LoginPage(page);
+//   await loginPage.enviarFormSesion(process.env.SAUCEDEMOUSER, process.env.SAUCEDEMOPASS);
+//   await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+// });
+
+
+
+
 import { test, expect } from '@playwright/test';
-const { LoginPage } = require('../pages/login-page');
-import dotenv from 'dotenv'
 
-dotenv.config()
+test('El usuario debe iniciar sesión correctamente en OrangeHRM', async ({ page }) => {
 
-test('El usuario debe iniciar sesión correctamente', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  await page.goto(process.env.SAUCEDEMOURL);
-  const loginPage = new LoginPage(page);
-  await loginPage.enviarFormSesion(process.env.SAUCEDEMOUSER, process.env.SAUCEDEMOPASS);
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  await page.getByPlaceholder('Username').fill('Admin');
+
+  await page.getByPlaceholder('Password').fill('admin123');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
 
 });
+
+// npx playwright test login.spec.js
